447df75b9625435a7704874440161b9cd02b24e4
Nikola Cvetkovic <ncvetkovic@tenstorrent.com>
2025-06-19 16:37:26 +0000

    #22944: Change tranpose_init, left transpose_tile

--- tt_metal/include/compute_kernel_api/transpose_wh.h
+++ tt_metal/include/compute_kernel_api/transpose_wh.h
@@ -14,33 +14,11 @@
 
 namespace ckernel {
 
-// clang-format off
 /**
- * Paired Init function for transpose_wh. For general information on init functions refer to any_init.
- *
- * | Argument       | Description                                                 | Type     | Valid Range | Required |
- * |----------------|-------------------------------------------------------------|----------|-------------|----------|
- * | icb            | The identifier of the circular buffer (CB) containing input | uint32_t | 0 to 31     | True     |
- */
- // clang-format on
-ALWI void transpose_wh_init(uint32_t icb, uint32_t ocb) {
-    MATH((llk_math_eltwise_unary_datacopy_init<A2D, DST_ACCUM_MODE, BroadcastType::NONE>(true, true, icb)));
-    MATH((llk_math_pack_sync_init<DST_ACCUM_MODE>()));
-    MATH((llk_math_hw_configure_disaggregated(icb, icb)));
-
-    PACK((llk_pack_hw_configure_disaggregated<DST_ACCUM_MODE, false>(ocb)));
-    PACK((llk_pack_init(ocb)));
-    PACK((llk_pack_dest_init<DST_ACCUM_MODE, false>()));
-
-    UNPACK((llk_unpack_A_hw_configure_disaggregated<DST_ACCUM_MODE>(icb, true)));
-    UNPACK((llk_unpack_A_init<BroadcastType::NONE, true, EltwiseBinaryReuseDestType::NONE>(true, true)));
-}
-
-/**
- * Performs a first-call or switch-from-another-op tile hw reconfiguration step needed for transpose_wh to be executed
+ * Performs a first-call or switch-from-another-op tile hw reconfiguration step needed for transpose op to be executed
  * correctly.
  */
-ALWI void transpose_wh_init_short(uint32_t icb) {
+ALWI void transpose_init(uint32_t icb) {
     MATH((llk_math_eltwise_unary_datacopy_init<A2D, DST_ACCUM_MODE, BroadcastType::NONE>(true, true, icb)));
 
     UNPACK((llk_unpack_A_init<BroadcastType::NONE, true, EltwiseBinaryReuseDestType::NONE>(true, true)));
@@ -64,7 +42,7 @@ ALWI void transpose_wh_init_short(uint32_t icb) {
  * | dst_tile_index | The index of the tile in DST REG for the result B       | uint32_t | Must be less than the acquired size of DST REG | True     |
  */
  // clang-format on
-ALWI void transpose_wh_tile(uint32_t icb, uint32_t itile, uint32_t idst) {
+ALWI void transpose_tile(uint32_t icb, uint32_t itile, uint32_t idst) {
     UNPACK((llk_unpack_A<BroadcastType::NONE, false>(icb, itile, false)));
     MATH((llk_math_eltwise_unary_datacopy<A2D, DST_ACCUM_MODE, BroadcastType::NONE>(idst)));
 }
